be26c32fbec761fee2f94ec6e3b08e44b863a785
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-30 19:51:11 -0400

    Add project files.

---
+++ Sheet4/Models/HomeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sheet4.Models
+{
+    enum Type{
+        TheMichealJackson,
+        ThePrince,
+        TheBackStreetBoys,
+        TheBeyonce,
+        TheBillieEilish,
+        TheBTS
+    };
+    public class HomeModel
+    {
+        private Type type { set; get; }
+    }
+}
