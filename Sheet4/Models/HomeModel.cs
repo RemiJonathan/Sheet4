b0135f54b5582e7aaaf3318111296a83626307e2
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-09-30 20:36:59 -0400

    Made Total Accessors

--- Sheet4/Models/HomeModel.cs
+++ Sheet4/Models/HomeModel.cs
@@ -30,8 +30,78 @@ namespace Sheet4.Models
     }
     public class HomeModel
     {
-        private Type type { set; get; }
-        private Size size { set; get; }
-        private Combo combo { set; get; }
+        private Type Type { set; get; }
+        private Size Size { set; get; }
+        private Combo Combo { set; get; }
+
+        static double GetTypePrice(Type type)
+        {
+            double price = 0;
+
+            switch (type)
+            {
+                case Type.TheBackStreetBoys:
+                    price = 12.95;
+                    break;
+                case Type.TheBeyonce:
+                    price = 22.95;
+                    break;
+                case Type.TheBillieEilish:
+                    price = 8.95;
+                    break;
+                case Type.TheBTS:
+                    price = 15.95;
+                    break;
+                case Type.TheMichealJackson:
+                    price = 18.95;
+                    break;
+                case Type.ThePrince:
+                    price = 64.95;
+                    break;
+            }
+
+            return price;
+        }
+
+        static double GetSizePrice(Size size)
+        {
+            double price = 0;
+
+            switch (size)
+            {
+                case Size.onehitwonder: break;
+                case Size.blist:
+                    price = 2.5;
+                    break;
+                case Size.alist:
+                    price = 5.0;
+                    break;
+                case Size.superstar:
+                    price = 9.65;
+                    break;
+            }
+
+            return price;
+        }
+
+        static double GetComboPrice(Combo combo)
+        {
+            double price = 0;
+
+            switch (combo)
+            {
+                case Combo.None:break;
+                case Combo.CookiesNDrink:
+                    price = 3.75;
+                    break;
+                case Combo.ChipsNDrink:
+                    price = 4.50;
+                    break;
+            }
+
+            return price;
+        }
+        //I have no clue what I am doing
+
     }
 }
